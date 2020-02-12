afb09dff704a1069e27a67a7824ef21accc7e950
AJ Clemente <areljann.c@gmail.com>
2020-02-11 22:58:21 -0500

    INIT: Initial project commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text>Test</Text>
+      <Text>Another Test</Text>
+    </View>
+  )
+}
+
+export default App
