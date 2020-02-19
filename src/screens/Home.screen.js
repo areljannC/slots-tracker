62a345fa8f37f90a4a04153ae4a9b146c41a73d4
AJ Clemente <areljann.c@gmail.com>
2020-02-18 22:06:13 -0500

    MISC: Test data fetching with hooks

--- src/screens/Home.screen.js
+++ src/screens/Home.screen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Text } from 'react-native-elements'
+import { Text, Button } from 'react-native-elements'
 
 const HomeScreen = () => {
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(res => res.json())
+      .then(res => {
+        setPosts([...posts, res.title])
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Home view</Text>
+      <Button title='Fetch Data' onPress={fetchPosts} />
+      {
+        posts.map(post => <Text>{post}</Text>)
+      }
     </View>
   )
 }
